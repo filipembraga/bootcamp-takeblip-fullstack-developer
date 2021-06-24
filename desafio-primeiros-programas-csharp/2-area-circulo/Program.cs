3ad1aaae12d8ee4424c6dd43f79105b444703f06
Filipe Braga <lipebraga16@hotmail.com>
2021-06-24 00:34:52 -0300

    Resolvendo o Desafio 2

---
+++ desafio-primeiros-programas-csharp/2-area-circulo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_area_circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            double radius;
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A=" + ( pi * Math.Pow(radius, 2)).ToString("0.0000"));
+        }
+    }
+}
