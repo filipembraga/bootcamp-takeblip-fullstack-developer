41fa63a1bae9390cc7a4dbf8d829e828f2fa4062
Filipe Braga <lipebraga16@hotmail.com>
2021-06-24 01:43:31 -0300

    Resolvendo o Desafio 5

---
+++ solucionando-desafios-csharp/5-conversao-tempo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_conversao_tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeInSeconds = int.Parse(Console.ReadLine());
+            int hours = timeInSeconds / 3600;
+            timeInSeconds = timeInSeconds % 3600; ;
+            int minutes = timeInSeconds / 60;
+            int seconds = timeInSeconds % 60;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+}
