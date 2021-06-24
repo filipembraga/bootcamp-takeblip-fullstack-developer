3afa70c865ccba17fd61ab68c5d9a222377905a5
Filipe Braga <lipebraga16@hotmail.com>
2021-06-24 01:33:43 -0300

    Resolvendo o Desafio 4

---
+++ solucionando-desafios-csharp/4-ultrapassando-v/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4_ultrapassando_v
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var R = int.Parse(Console.ReadLine());
+            var V = 0;
+            var valoresSomados = 0;
+            var qtdNumerosSomados = 1;
+
+            do {
+
+            V = int.Parse(Console.ReadLine());
+
+            } while ( R >= V );
+
+            for (valoresSomados = R; valoresSomados <= V; qtdNumerosSomados++) {
+                R = R + 1;
+                valoresSomados = valoresSomados + R;
+            }
+            Console.WriteLine(qtdNumerosSomados);
+        }
+    }
+}
