29d3b05a1a1dcf84445f174116c0213951309c57
Filipe Braga <lipebraga16@hotmail.com>
2021-06-24 00:41:17 -0300

    Resolvendo o Desafio 3

---
+++ desafio-primeiros-programas-csharp/3-multiplos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3_multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int firstNumber = int.Parse(values[0]);
+            int secondNumber = int.Parse(values[1]);
+
+            if (firstNumber % secondNumber == 0 || secondNumber % firstNumber == 0) {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
