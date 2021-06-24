83c5b0d8abb5372e710f14562e402d6d70307d2f
Filipe Braga <lipebraga16@hotmail.com>
2021-06-24 00:27:00 -0300

    Resolvendo o Desafio 1

---
+++ desafio-primeiros-programas-csharp/1-soma-simples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_soma_simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int SOMA = firstNumber + secondNumber;
+
+            Console.WriteLine("SOMA = {0}", SOMA);
+        }
+    }
+}
