ca82223b255e29fdcb3356bf838c1346bbdd42d3
Filipe Braga <lipebraga16@hotmail.com>
2021-06-29 02:52:11 -0300

    Resolvendo Desafio 2

---
+++ desafio-resolvendo-algoritmos-csharp/2-coxinha-de-bueno/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_coxinha_de_bueno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputValue = Console.ReadLine().Split(' ');
+            double numberOfDrumsticks = double.Parse(inputValue[0]);
+            double numberOfPeople = double.Parse(inputValue[1]);
+
+            Console.WriteLine("{0:0.00}",numberOfDrumsticks / numberOfPeople);
+
+        }
+    }
+}
