dcc0aabd7bac2bdd437bd87fb841bdbbffa9a922
Filipe Braga <lipebraga16@hotmail.com>
2021-06-29 03:12:06 -0300

    Resolvendo Desafio 3

---
+++ desafio-resolvendo-algoritmos-csharp/3-cardapio-aereo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3_cardapio_aereo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numberOfMealsAvailableLine = Console.ReadLine().Split(' ');
+            string[] numberOfOrdersLine = Console.ReadLine().Split(' ');
+            int peopleWithoutMeals = 0;
+
+            for(int x = 0; x<numberOfMealsAvailableLine.Length; x++){
+                int numberOfMealsAvailable = int.Parse(numberOfMealsAvailableLine[x]);
+                int numberOfOrders = int.Parse(numberOfOrdersLine[x]);
+
+                if (numberOfMealsAvailable < numberOfOrders){
+                    peopleWithoutMeals += (numberOfOrders-numberOfMealsAvailable);
+                }
+            }
+            Console.WriteLine("{0}", peopleWithoutMeals);
+
+        }
+    }
+}
